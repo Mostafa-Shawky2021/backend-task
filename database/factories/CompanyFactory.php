bce0505f488d510ee1c1f42bcb3cd91f476fd767
MostafaShawky <mostafashawky20177@gmail.com>
2023-08-17 01:33:06 +0300

    readme,model factory

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'company_name' => fake()->company,
+            'company_address' => fake()->address,
+            'company_logo' => fake()->image(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Employee;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\Company::factory(5)->has(Employee::factory()->count(3))->create();
+        \App\Models\Employee::factory(5)->create();
     }
 }
